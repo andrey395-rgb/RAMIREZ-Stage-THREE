1d87a9791ce580ff794731fede1f71a6d88bd22a
andrey395-rgb <chubbyramirez103@gmail.com>
2024-07-16 13:13:35 +0800

    Update ProjectCard.jsx

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -6,7 +6,7 @@ function ProjectCard({ id, avatar, first_name, last_name , email}) {
       <img id="imageholder" className="hover" src={avatar} />
       <h3 className="idlabel">#{id}</h3>
       <h4 className="labeltext">
-      {first_name},{last_name}
+      {first_name}, {last_name}
       </h4>
       <h3 className="emailadd">{email}</h3>
 
